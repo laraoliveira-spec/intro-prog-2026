09bee510f448df51f253da146a45f44d9934e35a
Lara <lara.oliveira@escolar.ifrn.edu.br>
2026-09-18 10:10:48 -0300

    aula dia 18/09

---
+++ assunto_listas/ex10.py
@@ -0,0 +1,7 @@
+quadrados = []
+for n in range(1, 11):
+    quadrados.append(n ** 2)
+print(quadrados)
+
+quadrados = [n**2 for n in range(1,11)]
+print(quadrados)
---
+++ assunto_listas/ex9.py
@@ -0,0 +1,3 @@
+carros = ["Gol", "Palio", "Fusca", "Chavette"]
+for n, carro in enumerate(carros):
+    print(f"Carro: {n+1}: {carro}")
